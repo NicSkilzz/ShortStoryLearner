e67488dc54ca6d722a1f1a548f5c6307f0491b4a
Nicolas Klin <nklin02@gmail.com>
2023-10-07 21:33:15 +0200

    Turn code into function and add lists of variables as output.

--- storylearner/utils/text_dir_iteration.py
+++ storylearner/utils/text_dir_iteration.py
@@ -2,14 +2,39 @@ import os
 #from pathlib import Path
 
 
-rel_path_directory = './raw_data'
+def read_text_files_from_directory(directory_path: str):
+    '''
+    Input: Path to directory containing only text files.
+    Ouptut: List of all filenames, and all cleaned texts.
+    '''
 
-for filename in os.listdir(rel_path_directory):
-    filepath = os.path.join(rel_path_directory, filename)
-    f = open(filepath, 'r')
-    content = f.read()
-    punc = '''!()-[]{};:'"\,<>./?@#$%^&*_~'''
-    clean_content = ''.join(character.lower() for character in content if character.isalpha() or character==' ')
-    content_list = clean_content.split()
-    print(len(content_list))
-    print(len(set(content_list)))
+    # Create empty lists for variables
+    list_of_all_filenames = []
+    list_of_all_cleaned_texts = []
+
+    for filename in os.listdir(directory_path):
+
+        #Read the file
+        filepath = os.path.join(directory_path, filename)
+        f = open(filepath, 'r')
+        content = f.read()
+
+        # Clean the text
+        ## by removing all characters except letters and selected characters in variable: important_characters
+        important_characters = ",. :"
+        text_only_letters_and_lower_case = ''.join(character for character in content if character.isalpha() or character in important_characters)
+
+        # Append current variables to corresponding lists
+        list_of_all_filenames.append(filename)
+        list_of_all_cleaned_texts.append(text_only_letters_and_lower_case)
+
+    return list_of_all_filenames, list_of_all_cleaned_texts
+
+
+
+
+
+
+
+#rel_path_directory = './raw_data'
+#print(read_text_files_from_directory(rel_path_directory))
