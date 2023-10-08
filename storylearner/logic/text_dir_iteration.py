8ac7b3cbf4e681f5c32974cbb6fb9939e122a578
Nicolas Klin <nklin02@gmail.com>
2023-10-08 16:16:03 +0200

    Create word_rarity_ranking.py and create function that creates a dataframe ranking all the words by their rarity in all the texts

---
+++ storylearner/logic/text_dir_iteration.py
@@ -0,0 +1,107 @@
+import os
+import pandas as pd
+
+
+def read_text_files_from_directory(directory_path: str):
+    '''
+    Input: Path to directory containing only text files.
+    Ouptut: List of all filenames in directory, list of all cleaned texts from files
+    -> return list:filenames, list:cleaned_texts
+    '''
+
+    # Create empty lists for variables
+    list_of_all_filenames = []
+    list_of_all_cleaned_texts = []
+
+    for filename in os.listdir(directory_path):
+
+        #Read the file
+        filepath = os.path.join(directory_path, filename)
+        f = open(filepath, 'r')
+        content = f.read()
+
+        # Clean the text
+        ## by removing all characters except letters and selected characters in variable: important_characters
+        important_characters = ",. :"
+        text_only_letters_and_lower_case = ''.join(
+            character for character in content if character.isalpha() or character in important_characters
+            )
+
+        # Append current variables to corresponding lists
+        list_of_all_filenames.append(filename)
+        list_of_all_cleaned_texts.append(text_only_letters_and_lower_case)
+
+    return list_of_all_filenames, list_of_all_cleaned_texts
+
+
+def turn_filenames_and_cleaned_texts_into_dataframe(list_of_all_filenames: list, list_of_all_cleaned_texts: list):
+    '''
+    Input: List of all filenames and list of all cleaned texts of those files.
+    Output: DataFrame with ... variables.
+    '''
+
+    dataframe_as_dictionary = {
+        'filenames': list_of_all_filenames,
+        'cleaned_text': list_of_all_cleaned_texts,
+    }
+
+    dataframe = pd.DataFrame(dataframe_as_dictionary)
+
+    return dataframe
+
+
+def add_describing_columns_to_dataframe(dataframe: pd.DataFrame):
+    '''
+    Input: DataFrame with columns named ("filenames", "cleaned_text")
+    Output: DataFrame with given and added columns
+    (
+        - "word_list",
+        - "word_count_with_duplicates",
+        - "word_count_without_duplicates",
+        )
+    '''
+
+    # Add word_list column
+    dataframe['word_list'] =  dataframe['cleaned_text'].apply(
+        lambda x: x.replace(',', ' ').replace('.', ' ').replace(':', ' ').lower().split()
+        )
+
+    # Add word word_count_with_duplicates column
+    dataframe['word_count_with_duplicates'] = len(dataframe['word_list'])
+
+    # Add word word_count_with_duplicates column
+    dataframe['word_count_without_duplicates'] = dataframe['word_list'].apply(
+        lambda x: len(set(x))
+            )
+
+    # Create function to calculate the average word length
+    def calculate_average_word_length(word_list: list):
+        '''
+        Input: word_list
+        Output: average word length
+        '''
+        # Calculate the total characters in the given text
+        total_characters = len(''.join(character for word in word_list for character in word))
+
+        # Calculate the total words in the given text
+        total_words = len(word_list)
+
+        # Calculate average
+        average_word_length = total_characters / total_words
+
+        return average_word_length
+
+    # Apply function to calculate the average word length to DataFrame
+    dataframe['average_word_length'] = dataframe['word_list'].apply(
+        lambda x: calculate_average_word_length(x)
+            )
+
+    return dataframe
+
+
+if __name__ == '__main__':
+    rel_path_directory = './raw_data'
+    list_of_all_filenames, list_of_all_cleaned_texts = read_text_files_from_directory(rel_path_directory)
+    dataframe = turn_filenames_and_cleaned_texts_into_dataframe(list_of_all_filenames, list_of_all_cleaned_texts)
+    new_dataframe = add_describing_columns_to_dataframe(dataframe)
+    print(new_dataframe.columns)
---
+++ storylearner/logic/word_rarity_calculator.py
@@ -0,0 +1,76 @@
+import pandas as pd
+
+from storylearner.logic.text_dir_iteration import read_text_files_from_directory, turn_filenames_and_cleaned_texts_into_dataframe, add_describing_columns_to_dataframe
+
+def create_total_word_list_with_duplicates(dataframe: pd.DataFrame):
+    '''
+    Input: DataFrame with a "word_list" column.
+    Output: List containing all the words from all the texts with duplicates.
+    '''
+    total_word_list_with_duplicates = [word for list in dataframe['word_list'] for word in list]
+
+    return total_word_list_with_duplicates
+
+def create_word_rarity_ranking(total_word_list_with_duplicates: list):
+    '''
+    Input: List of all the words from all the texts with duplicates
+    Output: DataFrame with columns ("word", "word_percentage", "ranking")
+    '''
+
+    # Calculate total_amount_words
+    total_amount_words = len(total_word_list_with_duplicates)
+
+    # Create word_list_without_duplicates
+    word_list_without_duplicates = list(set(total_word_list_with_duplicates))
+
+    # Create lists for columns: "word" and "word_percentage"
+    word_list = []
+    word_percentage_list = []
+
+    # Iterate through word_list_without_duplicates and calculate the percentage of each word
+    for current_word in word_list_without_duplicates:
+        if current_word not in word_list:
+            current_word_count = 0
+            for word in total_word_list_with_duplicates:
+                if word == current_word:
+                    current_word_count += 1
+
+            # Calculate current_word_percentage
+            current_word_percentage = current_word_count / total_amount_words
+
+            # Append current values to lists
+            word_list.append(current_word)
+            word_percentage_list.append(current_word_percentage)
+
+    # Create a temporary dictionary
+    word_rarity_df_as_dict = {
+        'word': word_list,
+        'word_percentage': word_percentage_list
+    }
+
+    # Turn dictionary into DataFrame
+    word_rarity_df = pd.DataFrame(word_rarity_df_as_dict)
+
+    # Sort dataframe by word_percentage (descending)
+    word_rarity_ranking_df = word_rarity_df.sort_values('word_percentage', ascending=False, ignore_index=True)
+
+    # Add ranking column to word_rarity_ranking_df
+    word_rarity_ranking_df['ranking'] = word_rarity_ranking_df.index + 1
+
+    return word_rarity_ranking_df
+
+
+
+if __name__ == '__main__':
+    # Create Test-DataFrame
+    rel_path_directory = './raw_data'
+    list_of_all_filenames, list_of_all_cleaned_texts = read_text_files_from_directory(rel_path_directory)
+    dataframe = turn_filenames_and_cleaned_texts_into_dataframe(list_of_all_filenames, list_of_all_cleaned_texts)
+    new_dataframe = add_describing_columns_to_dataframe(dataframe)
+
+    # Create total_word_list_with_duplicates
+    total_word_list_with_duplicates = create_total_word_list_with_duplicates(new_dataframe)
+    #print(len(total_word_list_with_duplicates))
+
+    word_rarity_ranking = create_word_rarity_ranking(total_word_list_with_duplicates)
+    print(word_rarity_ranking)
